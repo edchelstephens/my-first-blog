3c24f5c7b5016ea9b9f4b09a455252424a95cbb5
edchelstephen <edchelstephens@gmail.com>
2020-02-15 14:19:54 +0800

    Added comment support

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login, logout
 
@@ -58,3 +58,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
